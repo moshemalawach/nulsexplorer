7e5e554f2ab793a4726bf85a589e4fe5d3449d3e
Moshe Malawach <moshe.malawach@protonmail.com>
2018-07-09 15:16:54 +0200

    using cache on blocks stats for consensus page

--- nulsexplorer/web/controllers/main.py
+++ nulsexplorer/web/controllers/main.py
@@ -1,7 +1,9 @@
 import pkg_resources
 import aiohttp_jinja2
 
+from aiocache import cached, SimpleMemoryCache
 from aiohttp import web
+
 from nulsexplorer.web import app
 from nulsexplorer.model.consensus import Consensus
 from nulsexplorer.model.transactions import Transaction
@@ -24,19 +26,10 @@ async def index(request):
             'last_height': await get_last_block_height()}
 app.router.add_get('/', index)
 
-#@aiohttp_jinja2.template('search.html')
-
-    #return {'last_height': await get_last_block_height()}
-
-
-
-@aiohttp_jinja2.template('consensus.html')
-async def consensus(request):
-    """ Address view
-    """
-    last_height = await get_last_block_height()
-    consensus = await Consensus.collection.find_one(sort=[('height', -1)])
+@cached(ttl=60*10, cache=SimpleMemoryCache) # 600 seconds or 10 minutes
+async def get_packer_stats(last_height):
     packed_block_query = {'$group' : { '_id' : '$packingAddress', 'count' : {'$sum' : 1}}}
+
     totals = Block.collection.aggregate([packed_block_query])
     totals_all = {r['_id']: r['count'] async for r in totals}
     totals = Block.collection.aggregate([
@@ -49,9 +42,20 @@ async def consensus(request):
         packed_block_query
     ])
     totals_day = {r['_id']: r['count'] async for r in totals}
+
+    return (totals_all, totals_hour, totals_day)
+
+@aiohttp_jinja2.template('consensus.html')
+async def consensus(request):
+    """ Address view
+    """
+    last_height = await get_last_block_height()
+    consensus = await Consensus.collection.find_one(sort=[('height', -1)])
+
     #db.blocks.aggregate({$match: {'height': {'$gt': 40000}}},     )
     node_count = len(consensus['agents'])
     active_count = len([a for a in consensus['agents'] if a['status'] == 1])
+    totals_all, totals_hour, totals_day = await get_packer_stats(last_height)
 
     return {'consensus': consensus,
             'last_height': last_height,
