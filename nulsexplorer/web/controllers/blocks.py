abfcdcb9f8624ec6e5eba57b57adda1c4aa991d1
Moshe Malawach <moshe.malawach@protonmail.com>
2018-07-09 16:43:36 +0200

    adding pagination to block contents

--- nulsexplorer/web/controllers/blocks.py
+++ nulsexplorer/web/controllers/blocks.py
@@ -13,15 +13,22 @@ async def view_block(request):
     """
     block_hash = request.match_info['block_hash']
     block = await find_block({'hash': block_hash})
+    page = int(request.match_info.get('page', '1'))
     if block is None:
         raise web.HTTPNotFound(text="Block not found")
 
-    transactions = [item async for item in Transaction.find({'blockHeight': block['height']})]
+    transactions = [item async
+                    for item in Transaction.find({'blockHeight': block['height']},
+                                                 limit=PER_PAGE,
+                                                 skip=(page-1)*PER_PAGE)]
+    pagination = Pagination(page, PER_PAGE, block['txCount'])
 
     return {'block': block,
+            'pagination': pagination,
             'transactions': transactions,
             'last_height': await get_last_block_height()}
 app.router.add_get('/blocks/{block_hash}', view_block)
+app.router.add_get('/blocks/{block_hash}/page/{page}', view_block)
 
 @aiohttp_jinja2.template('blocks.html')
 async def block_list(request):
