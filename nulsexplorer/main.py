99f3ce4c17e32cdd2f0f8cbd7633c819d86520be
Moshe Malawach <moshe.malawach@protonmail.com>
2018-07-16 10:37:18 +0200

    removing unlock (bugged, should be fixed)

--- nulsexplorer/main.py
+++ nulsexplorer/main.py
@@ -74,8 +74,6 @@ async def check_blocks():
                     await store_block(block)
                     last_stored_height = block['height']
 
-                await Transaction.update_locks(last_height)
-
         await asyncio.sleep(8)
 
 async def worker():
