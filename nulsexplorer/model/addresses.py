5537ab19c96f390110b9dfdf077fb007c9b07583
Moshe Malawach <moshe.malawach@protonmail.com>
2018-07-17 11:22:12 +0200

    fixing bad collection name on addresses

--- nulsexplorer/model/addresses.py
+++ nulsexplorer/model/addresses.py
@@ -1,9 +1,6 @@
-
 import pymongo
 from nulsexplorer.model.base import BaseClass, Index
 
 class Address(BaseClass):
-    COLLECTION = "transactions"
+    COLLECTION = "addresses"
     INDEXES = [Index("hash", unique=True)]
-
-    
