5dbaad3a912b8e8230aa4c43bdddabd447d5d747
Moshe Malawach <moshe.malawach@protonmail.com>
2018-07-31 19:47:34 +0200

    ensure index on output status

--- nulsexplorer/model/blocks.py
+++ nulsexplorer/model/blocks.py
@@ -72,4 +72,5 @@ class Block(BaseClass):
     INDEXES = [Index("hash", unique=True),
                Index("height", pymongo.ASCENDING, unique=True),
                Index("height", pymongo.DESCENDING),
-               Index("packingAddress")]
+               Index("packingAddress"),
+               Index("outputs.status")]
