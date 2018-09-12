6fa7104a81446de64528354c3c6a988f617d28a0
Moshe Malawach <moshe.malawach@protonmail.com>
2018-09-12 17:10:59 +0200

    adding tx types to constant naming

--- nulsexplorer/__init__.py
+++ nulsexplorer/__init__.py
@@ -8,5 +8,10 @@ TRANSACTION_TYPES = {
     6: "cancel consensus",
     7: "yellow card",
     8: "red card",
-    9: "unregister consensus node"
+    9: "unregister consensus node",
+    10: "business data",
+    100: "create contract",
+    101: "call contract",
+    102: "delete contract",
+    103: "transfer contract"
 }
