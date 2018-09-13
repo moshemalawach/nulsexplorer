04326295f043e3dfcdf109be201f49f5e81ed715
Moshe Malawach <moshe.malawach@protonmail.com>
2018-09-13 17:39:01 +0200

    processor register

--- nulsexplorer/protocol/register.py
+++ nulsexplorer/protocol/register.py
@@ -2,5 +2,8 @@
 # We will register here handlers for the tx types
 TX_TYPES_REGISTER = dict()
 
+# We will register here processors for the tx by types
+TX_PROCESSOR_REGISTER = dict()
+
 def register_tx_type(tx_type, handler_class):
     TX_TYPES_REGISTER[tx_type] = handler_class
