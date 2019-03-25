1914104a2440fed15e3e11c94a4af0343912ed7b
Moshe Malawach <moshe.malawach@protonmail.com>
2019-03-26 00:38:18 +0100

    adding pin_add

--- nulsexplorer/modules/additional/ipfs.py
+++ nulsexplorer/modules/additional/ipfs.py
@@ -3,15 +3,16 @@ import asyncio
 import aiohttp
 import logging
 
-from nulsexplorer.modules.register import register_tx_type, register_tx_processor
+from nulsexplorer.modules.register import register_tx_processor
 
 LOGGER = logging.getLogger('ipfs_module')
+
 
 async def add_file(fileobject, filename):
     async with aiohttp.ClientSession() as session:
         from nulsexplorer.web import app
         url = "http://%s:%d/api/v0/add" % (app['config'].ipfs.host.value,
-                                 app['config'].ipfs.port.value)
+                                           app['config'].ipfs.port.value)
         data = aiohttp.FormData()
         data.add_field('path',
                        fileobject,
@@ -20,12 +21,14 @@ async def add_file(fileobject, filename):
         resp = await session.post(url, data=data)
         return await resp.json()
 
+
 async def get_ipfs_api():
     from nulsexplorer.web import app
     host = app['config'].ipfs.host.value
     port = app['config'].ipfs.port.value
 
     return ipfsapi.connect(host, port)
+
 
 async def get_json(hash):
     loop = asyncio.get_event_loop()
@@ -34,12 +37,22 @@ async def get_json(hash):
         None, api.get_json, hash)
     return result
 
+
+async def pin_add(hash):
+    loop = asyncio.get_event_loop()
+    api = await get_ipfs_api()
+    result = await loop.run_in_executor(
+        None, api.pin_add, hash)
+    return result
+
+
 async def add_json(value):
     loop = asyncio.get_event_loop()
     api = await get_ipfs_api()
     result = await loop.run_in_executor(
         None, api.add_json, value)
     return result
+
 
 async def process_transfer_ipfs_remark(tx):
     # This function takes a tx dict and modifies it in place.
